7d983b5599b9b46e5676f4bb280ef33dacb9a088
Rom <rom.valme@gmail.com>
2025-01-10 18:51:35 -0500

    improving get functionality

--- get_web_page.py
+++ get_web_page.py
@@ -1,15 +1,22 @@
 import socket
 
-HOST = 'cdr0mii.github.io'  # The server's hostname or IP address
+#HOST = 'cdr0mii.github.io'  # The server's hostname or IP address
 #HOST = 'google.com'  # The server's hostname or IP address
-PORT = 8080        # The port used by the server
+#HOST = "https:/www.google.com/search?q=food" #use query in url for search
+HOST = "google.com"
+PORT = 80       # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'GET /index.html HTTP/1.1\r\n\r\n')
+    s.sendall(b'GET /search?q=food HTTP/1.1\r\n\r\n') #format the search query
     #s.sendall(b'GET /index.html HTTP/1.1\r\n\r\n')
-    data = s.recv(2048)#header info
-    data2 = s.recv(2048)#message body
+    for x in range(20):
+        data = s.recv(4098)#header info
+        #print(data)
+        #if(b'Hilltop' in data):
+        print('Received', repr(data))
+        #print(data)
+        #data2 = s.recv(4098)#message body
 
 #import pdb;pdb.set_trace()
-print('Received', repr(data2))
+#print('Received', repr(data))
